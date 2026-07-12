eea2a68bd4934a1ea518be69eb527a04b0ac3fcd
joseling <joseling.gonzalez22@outlook.com>
2026-07-11 19:05:10 -0600

    It updated structure to site and some information

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const experienceBtn = document.getElementById('experienceBtn');
-const languageBtn = document.getElementById('languageBtn');
-const containerExperience = document.getElementById("containerExperience");
-const containerLanguages = document.getElementById("containerLanguages");
-const fill = document.getElementById('filler');
 const header = document.getElementById('header');
+const navBorderDinamic = document.getElementById('menu');
+const changeLogo = document.querySelector('#logo a img');
 const menuIcon = document.getElementById('menuIcon'); 
 const menuNav = document.getElementById('menu');
 const yearActual = document.getElementById('year');
@@ -11,87 +8,25 @@ const btnMenuHamburger = document.getElementById('hamburger-5');
 const menuOverlay = document.getElementById('menu-overlay');
 const navLink = document.querySelectorAll('.navLink');
 const containerNav = document.getElementById('container-nav');
-const timelineExperience = document.getElementById('timelineExperience');
-const timelineEducation = document.getElementById('timelineEducation');
 
 let year = new Date().getFullYear();
 
-experienceBtn.classList.toggle('active');
-containerLanguages.style.display = 'none';
+changeLogo.src = 'img/joseling-logo.png';
 yearActual.textContent = year;
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('activeScroll', window.scrollY > 0);
-    if(header.classList.contains('activeScroll'))
-    {
+    navBorderDinamic.classList.toggle('activeScroll', window.scrollY > 0);
+    
+    if(window.scrollY > 0)
+    {       
+        changeLogo.src = 'img/joseling-logo-blue.png';
         menuIcon.style.backgroundColor = 'white';
-    } else {
+    } else {       
+        changeLogo.src = 'img/joseling-logo.png';
         menuIcon.style.backgroundColor = 'transparent';
     }    
 });
-
-languageBtn.addEventListener('click', () => {     
-    if (languageBtn.classList.contains('active')) {
-         experienceBtn.classList.remove('active'); 
-    }
-    else {
-        fill.classList.toggle('active');
-        languageBtn.classList.toggle('active');
-        experienceBtn.classList.remove('active'); 
-        containerExperience.classList.remove('activeContainer');
-        containerExperience.style.display = 'none';
-        containerLanguages.style.display = 'flex';
-        containerLanguages.classList.toggle('activeContainer');
-    }
-});
-
-experienceBtn.addEventListener('click', () => {   
-    if(experienceBtn.classList.contains('active')) {
-        languageBtn.classList.remove('active');    
-    } else {
-        fill.classList.remove('active');
-        languageBtn.classList.remove('active');
-        experienceBtn.classList.toggle('active');   
-        containerLanguages.classList.remove('activeContainer');
-        containerLanguages.style.display = 'none';
-        fn_mqueryContainerProfessionalProfile();
-    }
-
-    function fn_mqueryContainerProfessionalProfile () {
-        const  mquerymovil = window.matchMedia('(max-width: 422px)');
-        const  mquerymovilUno = window.matchMedia('(min-width: 423) and (max-width: 449px)');
-        const  mquerymovilDos = window.matchMedia('(min-width: 450px) and (max-width: 500px)');
-        const mquerytablet= window.matchMedia('(min-width: 501px) and (max-width: 650px)');
-        const  mquerydesktop = window.matchMedia('(min-width: 1261px)');
-
-        if (mquerymovil.matches) 
-        {
-            timelineExperience.classList.toggle('movil');
-            timelineEducation.classList.toggle('movil');
-            containerExperience.style.display = 'block'
-        }
-        else if(mquerymovilUno.matches)
-        {           
-            timelineExperience.classList.toggle('movilUno');
-            timelineEducation.classList.toggle('movilUno');
-            containerExperience.style.display = 'block'
-        }
-        else if(mquerymovilDos.matches)
-        {
-            console.log("mquerymovilDos " + {mquerymovilDos})
-            timelineExperience.classList.toggle('movilDos');
-            timelineEducation.classList.toggle('movilDos');
-            containerExperience.style.display = 'block'
-        }
-        else if(mquerydesktop.matches)
-        {
-            containerExperience.style.display = 'flex';
-            containerExperience.classList.toggle('activeContainer');
-        }
-
-    }
-});
-
 
 btnMenuHamburger.addEventListener('click', () => {
     btnMenuHamburger.classList.toggle('is-active');
